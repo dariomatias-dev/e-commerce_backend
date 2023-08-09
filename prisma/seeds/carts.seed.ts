d84642a3507ce186de942760dc10a85f51da26f4
dario <matiasdario75@gmail.com>
2023-08-09 10:10:27 -0300

    feat(seed): added seeds to multiple tables
    - Added seed data for the following tables: PhysicalPersonUsers, LegalPersonUsers, Wishlists, Carts, Categories and Orders

---
+++ prisma/seeds/carts.seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+
+const seedCarts = async (prisma: PrismaClient) => {
+  await prisma.carts.deleteMany();
+
+  await Promise.all([
+    prisma.carts.create({
+      data: {
+        userId: '57e99e52-753e-4da7-8a67-a6286edd2ee4',
+        productIds: [],
+      },
+    }),
+    prisma.carts.create({
+      data: {
+        userId: 'f8a5ded4-9247-44c2-a794-15aa5ff6fda1',
+        productIds: [],
+      },
+    }),
+  ]);
+};
+
+export default seedCarts;
--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -1,10 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 import seedProducts from './products.seed';
+import seedPhysicalPersonUsers from './physicalPersonUsers.seed';
+import seedLegalPersonUsers from './legalPersonUsers.seed';
+import seedWishlists from './wishlists.seed';
+import seedCarts from './carts.seed';
+import seedCategories from './categories.seed';
+import seedOrders from './orders.seed';
 
 const prisma = new PrismaClient();
 
 async function run() {
+  await seedPhysicalPersonUsers(prisma);
+  await seedLegalPersonUsers(prisma);
+  await seedWishlists(prisma);
+  await seedCarts(prisma);
+  await seedCategories(prisma);
   await seedProducts(prisma);
+  await seedOrders(prisma);
 }
 
 run()

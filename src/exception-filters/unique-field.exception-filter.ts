92b7583eb876949c9b3e941bd2b5d7cfe830f46f
dario <matiasdario75@gmail.com>
2023-11-17 08:11:45 -0300

    feat: added new filter and improvements
    - Added unique field filter for the Prisma ORM error that occurs when trying to add a unique value that already exists in another record.
    - Fixed PrismaNotFoundExceptionFilter.
    - Improved main.ts file.

--- src/exception-filters/unique-field.exception-filter.ts
+++ src/exception-filters/unique-field.exception-filter.ts
@@ -3,21 +3,21 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { FastifyReply } from 'fastify';
 
 @Catch(PrismaClientKnownRequestError)
-export class PrismaNotFoundExceptionFilter implements ExceptionFilter {
+export class UniqueFieldExceptionFilter implements ExceptionFilter {
   catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
     const context = host.switchToHttp();
     const response = context.getResponse<FastifyReply>();
 
-    const messageError = exception?.meta.cause ?? exception.message;
+    const errorMessage = exception.meta?.cause ?? exception.message;
 
-    exception.code === 'P2025'
-      ? response.status(404).send({
-          statusCode: 404,
-          message: messageError,
+    exception.code === 'P2002'
+      ? response.status(409).send({
+          statusCode: 409,
+          errorMessage,
         })
-      : response.status(500).send({
-          statusCode: 500,
-          message: messageError,
+      : response.status(409).send({
+          statusCode: 409,
+          errorMessage,
         });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -8,17 +8,31 @@ import {
 import { AppModule } from './app.module';
 
 import { PrismaNotFoundExceptionFilter } from './exception-filters/prisma-not-found.exception-filter';
+import { UniqueFieldExceptionFilter } from './exception-filters/unique-field.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
-    { cors: true },
+    new FastifyAdapter({
+      logger: true,
+    }),
   );
 
-  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
-  app.useGlobalFilters(new PrismaNotFoundExceptionFilter());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
 
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+    }),
+  );
+
+  app.useGlobalFilters(
+    new PrismaNotFoundExceptionFilter(),
+    new UniqueFieldExceptionFilter(),
+  );
+
+  await app.listen(3003);
 }
 bootstrap();

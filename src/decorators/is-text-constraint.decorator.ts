46171dc04b42ec25679ef0944373c7950b83e74b
dario <matiasdario75@gmail.com>
2023-11-17 09:00:18 -0300

    feat: created the DTO of the individual user
    - DTO of the individual user created, with validations for each user field.

---
+++ src/decorators/is-text-constraint.decorator.ts
@@ -0,0 +1,31 @@
+import {
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  registerDecorator,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'customText', async: false })
+export class IsTextConstraint implements ValidatorConstraintInterface {
+  validate(text: string) {
+    const regex = /^[a-zA-Z\s]+$/;
+
+    return regex.test(text);
+  }
+
+  defaultMessage() {
+    return 'The field must contain only letters and spaces.';
+  }
+}
+
+export function IsText(validationOptions?: ValidationOptions) {
+  return (object: Record<string, any>, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsTextConstraint,
+    });
+  };
+}
--- src/physical-person-user/dto/create-physical-person-user.dto.ts
+++ src/physical-person-user/dto/create-physical-person-user.dto.ts
@@ -1,1 +1,91 @@
-export class CreatePhysicalPersonUserDto {}
+import {
+  IsBoolean,
+  IsDate,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+import { IsText } from 'src/decorators/is-text-constraint.decorator';
+
+export class CreatePhysicalPersonUserDto {
+  @MaxLength(24)
+  @MinLength(3)
+  @IsNotEmpty()
+  @IsString()
+  @IsText()
+  firstName: string;
+
+  @MaxLength(24)
+  @MinLength(3)
+  @IsNotEmpty()
+  @IsString()
+  @IsText()
+  lastName: string;
+
+  @IsDate()
+  dateOfBirth: Date;
+
+  @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, {
+    message: 'Invalid phone number',
+  })
+  @IsNotEmpty()
+  @IsString()
+  phone: string;
+
+  @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, {
+    message: 'Invalid CPF',
+  })
+  @IsNotEmpty()
+  @IsString()
+  cpf: string;
+
+  @IsNotEmpty()
+  @IsString()
+  rg: string;
+
+  @IsEmail()
+  email: string;
+
+  @MaxLength(100)
+  @MinLength(3)
+  @IsNotEmpty()
+  @IsString()
+  state: string;
+
+  @MaxLength(100)
+  @MinLength(3)
+  @IsNotEmpty()
+  @IsString()
+  city: string;
+
+  @MaxLength(255)
+  @MinLength(3)
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\d{5}-\d{3}$/, {
+    message: 'Invalid CEP',
+  })
+  cep: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Invalid password',
+  })
+  password: string;
+
+  @IsBoolean()
+  termsOfUse: boolean;
+
+  @IsBoolean()
+  receiveMessages: boolean;
+}
